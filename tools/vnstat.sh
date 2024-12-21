f77cca920d73cee996a8cebfa99d50e0fda59189
kipas77pro <lahbaru930@gmail.com>
2024-12-22 06:07:48 +0700

    Add files via upload

---
+++ tools/vnstat.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+NC='\e[0m'
+clear
+echo "Install Main..."
+echo "Progress..."
+sleep 2
+
+/etc/init.d/vnstat restart >/dev/null 2>&1
+wget -q https://humdi.net/vnstat/vnstat-2.6.tar.gz
+tar zxvf vnstat-2.6.tar.gz
+cd vnstat-2.6
+./configure --prefix=/usr --sysconfdir=/etc >/dev/null 2>&1 && make >/dev/null 2>&1 && make install >/dev/null 2>&1
+cd
+vnstat -u -i $NET
+sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
+chown vnstat:vnstat /var/lib/vnstat -R
+systemctl enable vnstat >/dev/null 2>&1
+/etc/init.d/vnstat restart >/dev/null 2>&1
+rm -f /root/vnstat-2.6.tar.gz >/dev/null 2>&1
+rm -rf /root/vnstat-2.6 >/dev/null 2>&1
+
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+yellow "Main successfully installed..."
+sleep 3
+clear
