805e023254e321653918a1112c2e77b3da4f36f4
kipas77pro <lahbaru930@gmail.com>
2025-04-24 13:41:39 +0700

    Add files via upload

--- upray.sh
+++ upray.sh
@@ -170,6 +170,7 @@ chmod +x /usr/local/bin/xray
 vless=$((RANDOM + 10000))
 vmess=$((RANDOM + 10000))
 trojanws=$((RANDOM + 10000))
+trojan-go=$((RANDOM + 10000))
 vlessgrpc=$((RANDOM + 10000))
 vmessgrpc=$((RANDOM + 10000))
 trojangrpc=$((RANDOM + 10000))
@@ -258,6 +259,19 @@ sed -i '$ iproxy_set_header Connection "upgrade";' /etc/nginx/conf.d/xray.conf
 sed -i '$ iproxy_set_header Host \$http_host;' /etc/nginx/conf.d/xray.conf
 sed -i '$ i}' /etc/nginx/conf.d/xray.conf
 
+#trgo
+sed -i '$ ilocation = /trojan-go' /etc/nginx/conf.d/xray.conf
+sed -i '$ i{' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_redirect off;' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_pass http://127.0.0.1:'"$trojan-go"';' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_http_version 1.1;' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_set_header X-Real-IP \$remote_addr;' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_set_header Upgrade \$http_upgrade;' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_set_header Connection "upgrade";' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_set_header Host \$http_host;' /etc/nginx/conf.d/xray.conf
+sed -i '$ i}' /etc/nginx/conf.d/xray.conf
+
 sed -i '$ ilocation ^~ /vless-grpc' /etc/nginx/conf.d/xray.conf
 sed -i '$ i{' /etc/nginx/conf.d/xray.conf
 sed -i '$ iproxy_redirect off;' /etc/nginx/conf.d/xray.conf
@@ -293,6 +307,124 @@ pelerr=$(cat /etc/xray/passwd)
 # set uuid xray
 uuid=$(cat /proc/sys/kernel/random/uuid)
 
+# Install Trojan Go
+latest_version="$(curl -s "https://api.github.com/repos/p4gefau1t/trojan-go/releases" | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+trojango_link="https://github.com/p4gefau1t/trojan-go/releases/download/v${latest_version}/trojan-go-linux-amd64.zip"
+mkdir -p "/usr/bin/trojan-go"
+mkdir -p "/etc/trojan-go"
+cd `mktemp -d`
+curl -sL "${trojango_link}" -o trojan-go.zip
+unzip -q trojan-go.zip && rm -rf trojan-go.zip
+mv trojan-go /usr/local/bin/trojan-go
+chmod +x /usr/local/bin/trojan-go
+mkdir /var/log/trojan-go/
+touch /etc/trojan-go/akun.conf
+touch /var/log/trojan-go/trojan-go.log
+
+# Buat Config Trojan Go
+cat > /etc/trojan-go/config.json << END
+{
+  "run_type": "server",
+  "local_addr": "0.0.0.0",
+  "local_port": $trojan-go,
+  "remote_addr": "127.0.0.1",
+  "remote_port": 89,
+  "log_level": 1,
+  "log_file": "/var/log/trojan-go/trojan-go.log",
+  "password": [
+      "$uuid"
+  ],
+  "disable_http_check": true,
+  "udp_timeout": 60,
+  "ssl": {
+    "verify": false,
+    "verify_hostname": false,
+    "cert": "/etc/xray/xray.crt",
+    "key": "/etc/xray/xray.key",
+    "key_password": "",
+    "cipher": "",
+    "curves": "",
+    "prefer_server_cipher": false,
+    "sni": "$domain",
+    "alpn": [
+      "http/1.1"
+    ],
+    "session_ticket": true,
+    "reuse_session": true,
+    "plain_http_response": "",
+    "fallback_addr": "127.0.0.1",
+    "fallback_port": 0,
+    "fingerprint": "firefox"
+  },
+  "tcp": {
+    "no_delay": true,
+    "keep_alive": true,
+    "prefer_ipv4": true
+  },
+  "mux": {
+    "enabled": false,
+    "concurrency": 8,
+    "idle_timeout": 60
+  },
+  "websocket": {
+    "enabled": true,
+    "path": "/trojango",
+    "host": "$domain"
+  },
+    "api": {
+    "enabled": false,
+    "api_addr": "",
+    "api_port": 0,
+    "ssl": {
+      "enabled": false,
+      "key": "",
+      "cert": "",
+      "verify_client": false,
+      "client_cert": []
+    }
+  }
+}
+END
+
+# Installing Trojan Go Service
+cat > /etc/systemd/system/trojan-go.service << END
+[Unit]
+Description=Trojan-Go Service By ETIL
+Documentation=https://wa.me/6287721815317
+After=network.target nss-lookup.target
+
+[Service]
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/local/bin/trojan-go -config /etc/trojan-go/config.json
+Restart=on-failure
+RestartPreventExitStatus=23
+
+[Install]
+WantedBy=multi-user.target
+END
+
+# Trojan Go Uuid
+cat > /etc/trojan-go/uuid.txt << END
+$uuid
+END
+
+# restart
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save
+netfilter-persistent reload
+systemctl daemon-reload
+systemctl stop trojan-go
+systemctl start trojan-go
+systemctl enable trojan-go
+systemctl restart trojan-go
+
+cd
+cp /root/domain /etc/xray
+
 # xray config
 cat <<EOF> /etc/xray/config.json
 {
@@ -321,24 +453,6 @@ cat <<EOF> /etc/xray/config.json
                {
                  "id": "${uuid}"                 
 #vless
-### Danii 2025-05-11
-},{"id": "f5b93d00-e37d-465f-9927-ff5f10e20547","email": "Danii"
-### hendroo 2025-05-11
-},{"id": "126cbbd5-5b02-47e4-a53c-bc9be0489f56","email": "hendroo"
-### ekoo 2025-05-11
-},{"id": "a6e24858-3b11-42ea-a515-73a38689b8e4","email": "ekoo"
-### kiswari 2025-05-09
-},{"id": "96850b5c-50f3-4e87-92ea-958e40c2c6ae","email": "kiswari"
-### kiaa 2025-05-09
-},{"id": "e887aeee-f664-4842-a459-ead502c4299d","email": "kiaa"
-### aman2 2025-05-09
-},{"id": "a2c93bdb-b476-4656-8911-607c2a072fd2","email": "aman2"
-### superturbo 2025-05-09
-},{"id": "13131846-f681-4a8e-883c-6bbf16548c05","email": "superturbo"
-### testing 2025-05-09
-},{"id": "1589ef8b-186f-4918-b2a2-66820b250b9d","email": "testing"
-### danii 2025-04-25
-},{"id": "9f679eb2-5f2e-451b-bca9-e602e50aafdc","email": "danii"
              }
           ]
        },
@@ -359,18 +473,6 @@ cat <<EOF> /etc/xray/config.json
                  "id": "${uuid}",
                  "alterId": 0
 #vmess
-### tesio 2025-04-11
-},{"id": "b4cb0ff1-1523-4e8c-bdfe-0a4c5484d777","alterId": 0,"email": "tesio"
-### tesssy 2025-04-11
-},{"id": "771a9572-49cb-485a-8c99-f92b05b79fd7","alterId": 0,"email": "tesssy"
-### meii 2025-04-25
-},{"id": "2a9a2213-1225-448d-bf57-2d183b8e4e3d","alterId": 0,"email": "meii"
-### jembut 2025-04-22
-},{"id": "e55e679a-b55e-4200-b921-37565527adfb","alterId": 0,"email": "jembut"
-### gopatrt 2025-04-13
-},{"id": "d0c8560e-7a9f-4844-b164-5a8416867932","alterId": 0,"email": "gopatrt"
-### neta 2025-11-16
-},{"id": "69288511-8fab-4249-86f4-65b427fabda8","alterId": 0,"email": "neta"
              }
           ]
        },
@@ -391,72 +493,6 @@ cat <<EOF> /etc/xray/config.json
               {
                  "password": "${uuid}"
 #trojanws
-### dani2 2025-05-11
-},{"password": "5cf6ee3d-d35e-4412-b285-1f066e93c469","email": "dani2"
-### yumar 2025-05-11
-},{"password": "78cff712-96ba-42f2-8952-7ab9518de995","email": "yumar"
-### bella 2025-05-10
-},{"password": "035b1dbc-b843-4488-a5df-21d24c9164ef","email": "bella"
-### ariin 2025-05-10
-},{"password": "31451e7c-fdc5-42ba-8b36-927016dd9add","email": "ariin"
-### dewiii 2025-05-10
-},{"password": "bbe3c299-e1a8-4233-8bd1-3e092933768f","email": "dewiii"
-### wasonoo 2025-05-09
-},{"password": "6fdd095f-1e27-492f-8a82-8495b29a6a17","email": "wasonoo"
-### lecongwe 2025-05-07
-},{"password": "b54f9970-ed0c-42e0-a4af-e7308b7dfa71","email": "lecongwe"
-### Anjasss 2025-05-07
-},{"password": "afe34ef2-ecfd-45e2-8a97-3aea8c02d6aa","email": "Anjasss"
-### tompel 2025-05-06
-},{"password": "d2977274-8c7c-4ce9-8eca-9bce544b78ee","email": "tompel"
-### rangga 2025-05-06
-},{"password": "e116ea49-faf3-460c-862b-8b8cfa247041","email": "rangga"
-### zainal 2025-05-05
-},{"password": "84e435aa-df7d-42ce-aa97-3f1d98ad3744","email": "zainal"
-### trojam2 2025-05-05
-},{"password": "ee6bd50f-9758-4f18-bacf-4da16a7f7d24","email": "trojam2"
-### dwi2 2025-05-03
-},{"password": "05e6e189-f12c-4646-88c8-0984adb17a3d","email": "dwi2"
-### bagas23 2025-05-02
-},{"password": "adaf3677-d99d-4da8-9e16-6fa35f9c9c5c","email": "bagas23"
-### Fika 2025-05-01
-},{"password": "f0dba9b6-d815-4c18-bce7-e96617bd229a","email": "Fika"
-### sintia 2025-05-02
-},{"password": "5d7c77a5-e4a0-4f76-9555-5d367c2e179d","email": "sintia"
-### gapett 2025-04-29
-},{"password": "2909811f-bb8d-402e-9ec1-7985a5a9b50e","email": "gapett"
-### banjarsari 2025-04-29
-},{"password": "8f0e53b8-7b04-4036-9d3a-bfc864aa2703","email": "banjarsari"
-### banymqs 2025-04-29
-},{"password": "7e766e73-e0ff-4795-b18a-cd8b8108dc75","email": "banymqs"
-### kuniii 2025-04-28
-},{"password": "04f4634c-50c3-4c7e-a8f3-e6f048807abc","email": "kuniii"
-### yakuza32 2025-04-28
-},{"password": "78f1c17c-e9ce-4716-b3be-cebb42dbed18","email": "yakuza32"
-### yakuza20 2025-04-28
-},{"password": "db8f8735-1483-49f4-81f4-a018d735d2e1","email": "yakuza20"
-### epiiikhr 2025-04-28
-},{"password": "9e0817de-d385-405d-97b7-f5156d160c2e","email": "epiiikhr"
-### Restii 2025-04-27
-},{"password": "8e861602-0020-491a-9c82-a113551f2955","email": "Restii"
-### tiaaa 2025-04-27
-},{"password": "5b168f76-9ae5-4d37-bc63-52e3b0f7e942","email": "tiaaa"
-### cctv12 2025-04-27
-},{"password": "b8d21f3a-0429-4f2d-a205-3d94398d5623","email": "cctv12"
-### epikahr 2025-04-27
-},{"password": "e7559d1d-282c-4f0c-87f1-0df1a72078fb","email": "epikahr"
-### gendut 2025-04-27
-},{"password": "ce5d9946-cfee-4ece-b5a9-586f6f272c62","email": "gendut"
-### lnaai3iiii9 2025-04-27
-},{"password": "d1c640a8-2c05-4f0f-8bc2-8be31ba2951e","email": "lnaai3iiii9"
-### saipul23 2025-04-27
-},{"password": "a8118c60-8090-45e0-8f58-e38101be1783","email": "saipul23"
-### ramadhan 2025-04-27
-},{"password": "515b1461-3718-4ad1-9bdc-4cfd7cf104d7","email": "ramadhan"
-### baru2 2025-04-27
-},{"password": "88676ca7-526a-46a0-a7cb-d2e88486dfd7","email": "baru2"
-### ipang 2025-06-12
-},{"password": "584ee28a-7260-44af-a2b7-48589034a41f","email": "ipang"
               }
           ],
          "udp": true
@@ -478,24 +514,6 @@ cat <<EOF> /etc/xray/config.json
              {
                "id": "${uuid}"
 #vlessgrpc
-### Danii 2025-05-11
-},{"id": "f5b93d00-e37d-465f-9927-ff5f10e20547","email": "Danii"
-### hendroo 2025-05-11
-},{"id": "126cbbd5-5b02-47e4-a53c-bc9be0489f56","email": "hendroo"
-### ekoo 2025-05-11
-},{"id": "a6e24858-3b11-42ea-a515-73a38689b8e4","email": "ekoo"
-### kiswari 2025-05-09
-},{"id": "96850b5c-50f3-4e87-92ea-958e40c2c6ae","email": "kiswari"
-### kiaa 2025-05-09
-},{"id": "e887aeee-f664-4842-a459-ead502c4299d","email": "kiaa"
-### aman2 2025-05-09
-},{"id": "a2c93bdb-b476-4656-8911-607c2a072fd2","email": "aman2"
-### superturbo 2025-05-09
-},{"id": "13131846-f681-4a8e-883c-6bbf16548c05","email": "superturbo"
-### testing 2025-05-09
-},{"id": "1589ef8b-186f-4918-b2a2-66820b250b9d","email": "testing"
-### danii 2025-04-25
-},{"id": "9f679eb2-5f2e-451b-bca9-e602e50aafdc","email": "danii"
              }
           ]
        },
@@ -516,18 +534,6 @@ cat <<EOF> /etc/xray/config.json
                  "id": "${uuid}",
                  "alterId": 0
 #vmessgrpc
-### tesio 2025-04-11
-},{"id": "b4cb0ff1-1523-4e8c-bdfe-0a4c5484d777","alterId": 0,"email": "tesio"
-### tesssy 2025-04-11
-},{"id": "771a9572-49cb-485a-8c99-f92b05b79fd7","alterId": 0,"email": "tesssy"
-### meii 2025-04-25
-},{"id": "2a9a2213-1225-448d-bf57-2d183b8e4e3d","alterId": 0,"email": "meii"
-### jembut 2025-04-22
-},{"id": "e55e679a-b55e-4200-b921-37565527adfb","alterId": 0,"email": "jembut"
-### gopatrt 2025-04-13
-},{"id": "d0c8560e-7a9f-4844-b164-5a8416867932","alterId": 0,"email": "gopatrt"
-### neta 2025-11-16
-},{"id": "69288511-8fab-4249-86f4-65b427fabda8","alterId": 0,"email": "neta"
              }
           ]
        },
@@ -548,72 +554,6 @@ cat <<EOF> /etc/xray/config.json
                {
                  "password": "${uuid}"
 #trojangrpc
-### dani2 2025-05-11
-},{"password": "5cf6ee3d-d35e-4412-b285-1f066e93c469","email": "dani2"
-### yumar 2025-05-11
-},{"password": "78cff712-96ba-42f2-8952-7ab9518de995","email": "yumar"
-### bella 2025-05-10
-},{"password": "035b1dbc-b843-4488-a5df-21d24c9164ef","email": "bella"
-### ariin 2025-05-10
-},{"password": "31451e7c-fdc5-42ba-8b36-927016dd9add","email": "ariin"
-### dewiii 2025-05-10
-},{"password": "bbe3c299-e1a8-4233-8bd1-3e092933768f","email": "dewiii"
-### wasonoo 2025-05-09
-},{"password": "6fdd095f-1e27-492f-8a82-8495b29a6a17","email": "wasonoo"
-### lecongwe 2025-05-07
-},{"password": "b54f9970-ed0c-42e0-a4af-e7308b7dfa71","email": "lecongwe"
-### Anjasss 2025-05-07
-},{"password": "afe34ef2-ecfd-45e2-8a97-3aea8c02d6aa","email": "Anjasss"
-### tompel 2025-05-06
-},{"password": "d2977274-8c7c-4ce9-8eca-9bce544b78ee","email": "tompel"
-### rangga 2025-05-06
-},{"password": "e116ea49-faf3-460c-862b-8b8cfa247041","email": "rangga"
-### zainal 2025-05-05
-},{"password": "84e435aa-df7d-42ce-aa97-3f1d98ad3744","email": "zainal"
-### trojam2 2025-05-05
-},{"password": "ee6bd50f-9758-4f18-bacf-4da16a7f7d24","email": "trojam2"
-### dwi2 2025-05-03
-},{"password": "05e6e189-f12c-4646-88c8-0984adb17a3d","email": "dwi2"
-### bagas23 2025-05-02
-},{"password": "adaf3677-d99d-4da8-9e16-6fa35f9c9c5c","email": "bagas23"
-### Fika 2025-05-01
-},{"password": "f0dba9b6-d815-4c18-bce7-e96617bd229a","email": "Fika"
-### sintia 2025-05-02
-},{"password": "5d7c77a5-e4a0-4f76-9555-5d367c2e179d","email": "sintia"
-### gapett 2025-04-29
-},{"password": "2909811f-bb8d-402e-9ec1-7985a5a9b50e","email": "gapett"
-### banjarsari 2025-04-29
-},{"password": "8f0e53b8-7b04-4036-9d3a-bfc864aa2703","email": "banjarsari"
-### banymqs 2025-04-29
-},{"password": "7e766e73-e0ff-4795-b18a-cd8b8108dc75","email": "banymqs"
-### kuniii 2025-04-28
-},{"password": "04f4634c-50c3-4c7e-a8f3-e6f048807abc","email": "kuniii"
-### yakuza32 2025-04-28
-},{"password": "78f1c17c-e9ce-4716-b3be-cebb42dbed18","email": "yakuza32"
-### yakuza20 2025-04-28
-},{"password": "db8f8735-1483-49f4-81f4-a018d735d2e1","email": "yakuza20"
-### epiiikhr 2025-04-28
-},{"password": "9e0817de-d385-405d-97b7-f5156d160c2e","email": "epiiikhr"
-### Restii 2025-04-27
-},{"password": "8e861602-0020-491a-9c82-a113551f2955","email": "Restii"
-### tiaaa 2025-04-27
-},{"password": "5b168f76-9ae5-4d37-bc63-52e3b0f7e942","email": "tiaaa"
-### cctv12 2025-04-27
-},{"password": "b8d21f3a-0429-4f2d-a205-3d94398d5623","email": "cctv12"
-### epikahr 2025-04-27
-},{"password": "e7559d1d-282c-4f0c-87f1-0df1a72078fb","email": "epikahr"
-### gendut 2025-04-27
-},{"password": "ce5d9946-cfee-4ece-b5a9-586f6f272c62","email": "gendut"
-### lnaai3iiii9 2025-04-27
-},{"password": "d1c640a8-2c05-4f0f-8bc2-8be31ba2951e","email": "lnaai3iiii9"
-### saipul23 2025-04-27
-},{"password": "a8118c60-8090-45e0-8f58-e38101be1783","email": "saipul23"
-### ramadhan 2025-04-27
-},{"password": "515b1461-3718-4ad1-9bdc-4cfd7cf104d7","email": "ramadhan"
-### baru2 2025-04-27
-},{"password": "88676ca7-526a-46a0-a7cb-d2e88486dfd7","email": "baru2"
-### ipang 2025-06-12
-},{"password": "584ee28a-7260-44af-a2b7-48589034a41f","email": "ipang"
                }
            ]
         },
