a64f3e2fa046bd8d7d2813c30618c62900619da4
kipas77pro <lahbaru930@gmail.com>
2025-04-25 08:48:07 +0700

    Add files via upload

--- add/menu.sh
+++ add/menu.sh
@@ -9,8 +9,8 @@ ssh1="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
 #let tra=$trx/2
 #ssx=$(grep -c -E "^## " "/etc/xray/config.json")
 #let ssa=$ssx/2
-trx=$(grep -c -E "^### " "/etc/trojan-go/config.json")
-let tra=$trx/3
+trgo=$(grep -c -E "^### " "/etc/trojan-go/config.json")
+let tra=$trgo
 
 NC='\033[0;37m'
 BICyan='\033[0;34m'
