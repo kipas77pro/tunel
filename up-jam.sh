3b19c70a465af0f9b45ce9ec6dc4d81dbb74a138
kipas77pro <lahbaru930@gmail.com>
2025-05-05 03:08:25 +0700

    Add files via upload

---
+++ up-jam.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cat > /etc/cron.d/xp_otm <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+2 0 * * * root /usr/bin/xp
+END
+cat > /etc/cron.d/cl_otm <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+2 1 * * * root /usr/bin/clearlog
+END
+cat > /etc/cron.d/re_otm <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 5 * * * root /usr/bin/reboot
+END
+#cat > /home/re_otm <<-END
+#05
+#END
+service cron restart >/dev/null 2>&1
+service cron reload >/dev/null 2>&1
