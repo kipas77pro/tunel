bab09e9a9157a04aecebcdbaa66ddeb23b3376c1
kipas77pro <lahbaru930@gmail.com>
2025-05-06 17:35:43 +0700

    Add files via upload

--- up.sh
+++ up.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# Install Xray #
-# / / Ambil Xray Core Version Terbaru
-latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
-# / / Installation Xray Core
-xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v$latest_version/xray-linux-64.zip"
-# / / Ambil Xray Core Version Terbaru
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.8.16
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.8.17
 
-# Random Port Xray
 # / / Make Main Directory
 mkdir -p /usr/bin/xray
 mkdir -p /etc/xray
