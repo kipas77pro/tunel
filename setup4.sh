236dd396e115603d3e57338ace7dd45e05ebb602
kipas77pro <lahbaru930@gmail.com>
2025-05-09 02:53:08 +0700

    Add files via upload

--- setup4.sh
+++ setup4.sh
@@ -419,7 +419,7 @@ echo  "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
 echo  "   - Dflate                  : [ON]"  | tee -a log-install.txt
 echo  "   - IPtables                : [ON]"  | tee -a log-install.txt
 echo  "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
-echo  "   - Autoreboot              : 00.00 GMT +7" | tee -a log-install.txt
+echo  "   - Autoreboot              : 05.00 GMT +7" | tee -a log-install.txt
 echo  "   - AutoKill Multi Login User" | tee -a log-install.txt
 echo  "   - Auto Delete Expired Account" | tee -a log-install.txt
 echo  "   - Fully automatic script" | tee -a log-install.txt
