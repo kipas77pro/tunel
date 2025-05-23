efa2e264a6d9b49dd23bdc12f90e3b648274677f
kipas77pro <lahbaru930@gmail.com>
2025-05-23 10:23:02 +0700

    Add files via upload

---
+++ baner.sh
@@ -0,0 +1,22 @@
+<p style="text-align:center"><b>
+<br><big><big><big><big><u><font color="#ff0000">I
+</font><small><font color="#ed1211">N </font><small><font
+color="#db2423">D </font><small><font color="#c83734">O
+</font><small><font color="#b64946">R </font><font
+color="#a45b57">A </font><font color="#926d69">J
+</font><font color="#6d928b">A </font><font
+color="#49b6ae">M </font><big><font color="#37c8c0">I
+</font><big><font color="#24dbd1">T </font><big><font
+color="#12ede3">R </font><big><font color="#00fff4">A              </u></font><font><small><small><small><small><small><br><br><font
+color="#ff0000"><u>》》TERM OF SERVICE !!《《</u><small>
+</font><br><big><font color="#ff7f00">No Ddos, No
+Hacking</font><br><font color="#ffff00">Multi Login sesuai         Account</font><br><font color="#00ff00">No Ilegal aktivity
+lainnya.</font><font
+color="#fdfdfd"><font><small><big><big>‎<big>‎</font>
+<br><br><font color="#00fff4">‎‎ ‎ ‎‎ ‎‎ ‎‎‎<u>Hubungi Kami
+</u></font><big><font color="#fdfdfd">୧⍤⃝</font><font
+color="#1ff61f">ᯤ‎‎ ‎‎ ‎‎</font>
+<br>‎<small><small><small><small><font color="#fdfdfd"> ‎ ‎‎ ‎‎ ‎‎ ‎‎
+DEKENGANE PUSAT BLITAR <br>‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎RESMI SNI INDONESIA
+ ‎</a>‎<br>‎‎ ‎‎◇━━━━━━━━━━━━━━━━━━━━━━━◇<br></font><br><font
+color="FFFF00">Rebot Server Jam 5 Pagi</font><br>
