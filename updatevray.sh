f040ce08d48c8ba10e042b1d66415bc0244070e1
kipas77pro <lahbaru930@gmail.com>
2025-05-30 09:52:42 +0700

    Add files via upload

--- updatevray.sh
+++ updatevray.sh
@@ -82,7 +82,7 @@ domainSock_dir="/run/xray";! [ -d $domainSock_dir ] && mkdir  $domainSock_dir
  chown www-data.www-data $domainSock_dir
 
 latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.8.23
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.8.19
  
 wget -O /etc/xray/config.json "${REPO}media/config.json" >/dev/null 2>&1
 wget -O /etc/systemd/system/runn.service "${REPO}media/runn.service" >/dev/null 2>&1
