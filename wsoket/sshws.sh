8aed759ed578f67bd2b5ff6deadaa4add8f19b57
kipas77pro <lahbaru930@gmail.com>
2025-06-12 13:28:13 +0700

    Add files via upload

---
+++ wsoket/sshws.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+curl -sS ipv4.icanhazip.com > /usr/bin/.ipvps
+REPO="https://raw.githubusercontent.com/kipas77pro/tunel/main/wsoket/main/"
+wget -O /usr/bin/ws "${REPO}ws"
+wget -O /usr/bin/config.conf "${REPO}config.conf"
+wget -O /etc/systemd/system "${REPO}ws.service"
+chmod +x /usr/bin/ws
+systemctl daemon-reload
+systemctl enable ws.service
+systemctl start ws.service
+systemctl restart ws.service
